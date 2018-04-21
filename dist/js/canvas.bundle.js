d0af821d23598233c2686aebba5ad2b1f59c98f4
Paul <pfreema1@gmail.com>
2018-04-21 12:58:34 -0400

    init

--- dist/js/canvas.bundle.js
+++ dist/js/canvas.bundle.js
@@ -82,80 +82,85 @@ canvas.height = innerHeight;
 
 // Variables
 var mouse = {
-    x: innerWidth / 2,
-    y: innerHeight / 2
+  x: innerWidth / 2,
+  y: innerHeight / 2
 };
 
 var colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66'];
 
 // Event Listeners
 addEventListener('mousemove', function (event) {
-    mouse.x = event.clientX;
-    mouse.y = event.clientY;
+  mouse.x = event.clientX;
+  mouse.y = event.clientY;
 });
 
 addEventListener('resize', function () {
-    canvas.width = innerWidth;
-    canvas.height = innerHeight;
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
 
-    init();
+  init();
 });
 
 // Utility Functions
 function randomIntFromRange(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function randomColor(colors) {
-    return colors[Math.floor(Math.random() * colors.length)];
+  return colors[Math.floor(Math.random() * colors.length)];
 }
 
 function distance(x1, y1, x2, y2) {
-    var xDist = x2 - x1;
-    var yDist = y2 - y1;
+  var xDist = x2 - x1;
+  var yDist = y2 - y1;
 
-    return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
+  return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
 }
 
 // Objects
 function Object(x, y, radius, color) {
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.color = color;
+  this.x = x;
+  this.y = y;
+  this.radius = radius;
+  this.color = color;
 }
 
 Object.prototype.update = function () {
-    this.draw();
+  this.draw();
 };
 
 Object.prototype.draw = function () {
-    c.beginPath();
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    c.fillStyle = this.color;
-    c.fill();
-    c.closePath();
+  c.beginPath();
+  c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+  c.fillStyle = this.color;
+  c.fill();
+  c.closePath();
 };
 
 // Implementation
 var objects = void 0;
 function init() {
-    objects = [];
+  objects = [];
 
-    for (var i = 0; i < 400; i++) {
-        // objects.push();
-    }
+  for (var i = 0; i < 400; i++) {
+    var foo = new Object(Math.random() * canvas.width, Math.random() * canvas.height, Math.random() * 15, 'yellow');
+    objects.push(foo);
+  }
+
+  console.log(objects);
 }
 
 // Animation Loop
 function animate() {
-    requestAnimationFrame(animate);
-    c.clearRect(0, 0, canvas.width, canvas.height);
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  c.fillStyle = 'blue';
+  c.fillRect(0, 0, canvas.width, canvas.height);
 
-    c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y);
-    // objects.forEach(object => {
-    //  object.update();
-    // });
+  objects.forEach(function (object) {
+    // console.log('wtf');
+    object.update();
+  });
 }
 
 init();
